4bfcaf263fe6d6e55b2a7bd19ed6eb407a5896d6
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-04-14 16:10:42 +0300

    Create
    Create

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+//style
+import "./style.scss"
+//components
+import Register from "./pages/Register"
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
