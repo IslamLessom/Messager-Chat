57ddafafecac1d5527b1adcdc186c3d00071341f
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2023-05-02 23:47:28 +0300

    Add files via upload

--- src/pages/Friends.elements.jsx
+++ src/pages/Friends.elements.jsx
@@ -19,4 +19,49 @@ export const Container = styled.div`
 
 export const FriendsContent = styled.div`
   flex: 2;
+  display: grid;
+  grid-template-columns: 220px 220px 220px;
+  background-color: rgb(221, 221, 247);
+  justify-content: center;
+  padding-top: 30px;
+  padding-bottom: 30px;
+
+  overflow: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+`;
+export const FriendsBlock = styled.div`
+  position: relative;
+  padding: 15px;
+  background-color: #5d5b8d;
+  border-radius: 10px;
+  text-align: center;
+  height: 250px;
+  width: 180px;
+  margin: 10px;
+  color: white;
+`;
+export const ImagesBlock = styled.div`
+  background: white;
+  padding: 8px;
+  border-radius: 8px;
+`;
+export const Images = styled.img`
+  width: 150px;
+  border-radius: 8px;
+  height: 120px;
+`;
+export const Name = styled.h3`
+  position: absolute;
+  bottom: 55px;
+  margin-left: 18px;
+`;
+export const City = styled.p`
+  position: absolute;
+  bottom: 25px;
+  margin-left: 58px;
 `;
--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -2,14 +2,108 @@ import React from "react";
 //components
 import Sidebar from "../components/Sidebar";
 //styled
-import { Container, FriendsContent, FriendsS } from "./Friends.elements";
+import {
+  City,
+  Container,
+  FriendsBlock,
+  FriendsContent,
+  FriendsS,
+  Images,
+  ImagesBlock,
+  Name,
+} from "./Friends.elements";
 
 function Friends() {
   return (
     <FriendsS>
       <Container>
         <Sidebar />
-        <FriendsContent></FriendsContent>
+        <FriendsContent>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOwr7UvG9cTfF_AiYGX0mZC_kmiGCNS2N3VwF9RDi7V1UqMj8js0eux9X_aACTbFNpY84&usqp=CAU" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOwr7UvG9cTfF_AiYGX0mZC_kmiGCNS2N3VwF9RDi7V1UqMj8js0eux9X_aACTbFNpY84&usqp=CAU" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://unsplash-assets.imgix.net/marketing/community/photographer01.jpg?auto=format&fit=crop&q=60" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://media.istockphoto.com/id/497276084/photo/guy-on-a-background-of-the-sun.jpg?s=170667a&w=0&k=20&c=lZstKV9gXp3uaa2mTd5fsYmg-SxnTOjA49SFO_b9--k=" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOwr7UvG9cTfF_AiYGX0mZC_kmiGCNS2N3VwF9RDi7V1UqMj8js0eux9X_aACTbFNpY84&usqp=CAU" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOwr7UvG9cTfF_AiYGX0mZC_kmiGCNS2N3VwF9RDi7V1UqMj8js0eux9X_aACTbFNpY84&usqp=CAU" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://unsplash-assets.imgix.net/marketing/community/photographer01.jpg?auto=format&fit=crop&q=60" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+          <FriendsBlock>
+            <ImagesBlock>
+              <Images src="https://media.istockphoto.com/id/497276084/photo/guy-on-a-background-of-the-sun.jpg?s=170667a&w=0&k=20&c=lZstKV9gXp3uaa2mTd5fsYmg-SxnTOjA49SFO_b9--k=" />
+            </ImagesBlock>
+            <Name>Гасанов Ислам</Name>
+            <City>Г.Кизляр</City>
+          </FriendsBlock>
+        </FriendsContent>
       </Container>
     </FriendsS>
   );
