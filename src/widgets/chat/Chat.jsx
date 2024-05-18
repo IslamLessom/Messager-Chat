886e2564f432e197c205c5eba5b4a1e8a70be89d
IslamProg <74923378+IslamLessom@users.noreply.github.com>
2024-05-18 11:58:06 +0300

    new version / mobile

--- src/widgets/chat/Chat.jsx
+++ src/widgets/chat/Chat.jsx
@@ -13,6 +13,7 @@ import { useChatStore } from "../../lib/chatStore";
 import { useUserStore } from "../../lib/userStore";
 import upload from "../../lib/upload";
 import { format } from "timeago.js";
+import Detail from "../detail/Detail";
 
 
 const Chat = () => {
@@ -103,100 +104,111 @@ const Chat = () => {
       });
     } catch (err) {
       console.log(err);
-    } finally{
-    setImg({
-      file: null,
-      url: "",
-    });
+    } finally {
+      setImg({
+        file: null,
+        url: "",
+      });
 
-    setText("");
+      setText("");
     }
   };
 
   return (
-    <div className="chat">
-      <div className="top">
-        <div className="user">
-          <img src={user?.avatar || "./avatar.png"} alt="" />
-          <div className="texts">
-            <span>{user?.username}</span>
-            <p>Lorem ipsum dolor, sit amet.</p>
-          </div>
-        </div>
-        <div className="icons">
-          <img src="./phone.png" alt="" />
-          <img src="./video.png" alt="" />
-          <button  className="info-button"><img src="./info.png" alt="" /></button>
-        </div>
-      </div>
-      <div className="center">
-        {chat?.messages?.map((message) => (
-          <div
-            className={
-              message.senderId === currentUser?.id ? "message own" : "message"
-            }
-            key={message?.createAt}
-          >
-            <div className="texts">
-              {message.img && <img src={message.img} alt="" />}
-              <p>{message.text}</p>
-              <span>{format(message.createdAt.toDate())}</span>
+    <>
+      <div className="chat">
+        <div>
+
+          <div className="top">
+            <div className="user">
+              <img src={user?.avatar || "./avatar.png"} alt="" />
+              <div className="texts">
+                <span>{user?.username}</span>
+                <p>Lorem ipsum dolor, sit amet.</p>
+              </div>
+            </div>
+            <div className="icons">
+              <img src="./phone.png" alt="" />
+              <img src="./video.png" alt="" />
+              <img
+                src="./info.png"
+                alt=""
+                onClick={() => setDetails((prev) => !prev)}
+              />
             </div>
           </div>
-        ))}
-        {img.url && (
-          <div className="message own">
-            <div className="texts">
-              <img src={img.url} alt="" />
+          <div className="center">
+            {chat?.messages?.map((message) => (
+              <div
+                className={
+                  message.senderId === currentUser?.id ? "message own" : "message"
+                }
+                key={message?.createAt}
+              >
+                <div className="texts">
+                  {message.img && <img src={message.img} alt="" />}
+                  <p>{message.text}</p>
+                  <span>{format(message.createdAt.toDate())}</span>
+                </div>
+              </div>
+            ))}
+            {img.url && (
+              <div className="message own">
+                <div className="texts">
+                  <img src={img.url} alt="" />
+                </div>
+              </div>
+            )}
+            <div ref={endRef}></div>
+          </div>
+          <div className="bottom">
+            <div className="icons">
+              <label htmlFor="file">
+                <img src="./img.png" alt="" />
+              </label>
+              <input
+                type="file"
+                id="file"
+                style={{ display: "none" }}
+                onChange={handleImg}
+              />
+              <img src="./camera.png" alt="" />
+              <img src="./mic.png" alt="" />
             </div>
-          </div>
-        )}
-        <div ref={endRef}></div>
-      </div>
-      <div className="bottom">
-        <div className="icons">
-          <label htmlFor="file">
-            <img src="./img.png" alt="" />
-          </label>
-          <input
-            type="file"
-            id="file"
-            style={{ display: "none" }}
-            onChange={handleImg}
-          />
-          <img src="./camera.png" alt="" />
-          <img src="./mic.png" alt="" />
-        </div>
-        <input
-          type="text"
-          placeholder={
-            isCurrentUserBlocked || isReceiverBlocked
-              ? "You cannot send a message"
-              : "Type a message..."
-          }
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          disabled={isCurrentUserBlocked || isReceiverBlocked}
-        />
-        <div className="emoji">
-          <img
-            src="./emoji.png"
-            alt=""
-            onClick={() => setOpen((prev) => !prev)}
-          />
-          <div className="picker">
-            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+            <input
+              type="text"
+              placeholder={
+                isCurrentUserBlocked || isReceiverBlocked
+                  ? "You cannot send a message"
+                  : "Type a message..."
+              }
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              disabled={isCurrentUserBlocked || isReceiverBlocked}
+            />
+            <div className="emoji">
+              <img
+                src="./emoji.png"
+                alt=""
+                onClick={() => setOpen((prev) => !prev)}
+              />
+              <div className="picker">
+                <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+              </div>
+            </div>
+            <button
+              className="sendButton"
+              onClick={handleSend}
+              disabled={isCurrentUserBlocked || isReceiverBlocked}
+            >
+              Send
+            </button>
           </div>
         </div>
-        <button
-          className="sendButton"
-          onClick={handleSend}
-          disabled={isCurrentUserBlocked || isReceiverBlocked}
-        >
-          Send
-        </button>
+        {details && <Detail />}
       </div>
-    </div>
+
+    </>
   );
 };
 
